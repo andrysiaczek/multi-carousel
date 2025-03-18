a62707a7bb6fa26b8c2474aca416a82545c16618
andrysiaczek <agata.and.11@gmail.com>
2025-03-18 12:11:29 +0100

    Set up tailwind with base configuration and a custom color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        antiflashWhite: '#F5F6FA',
+        lightGray: '#DEDEDE',
+        darkGray: '#898989',
+        lightGreen: '#BCDEDC',
+        darkGreen: '#006D77',
+        lightOrange: '#FFE6DE',
+        darkOrange: '#E29578',
+      },
+    },
   },
   plugins: [],
-}
-
+};
